3af9b6a61a936583c15681d6e8f88aa2dc7385c0
prateekj07394 <prateekj07394@gmail.com>
2020-01-28 23:07:30 +0530

    boilerplate code

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry : path.join(__dirname,"src","public","js","index.js"),
+    output : {
+        path : path.join(__dirname,"dist"),
+        filename : "bundle.js"
+    },
+    mode:"development",
+    module : {
+        rules : [
+            {
+                test : /\.jsx?/,
+                exclude : /node_modules/,
+                loader : "babel-loader",
+                query : {
+                    presets : ["@babel/preset-react"]
+                }
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template : path.join(__dirname,"src","views","index.html")
+        })
+    ]
+}
